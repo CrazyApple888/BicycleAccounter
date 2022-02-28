89f22e44ed78089ea66a4152328455e9d20055d6
Andrey Isachenko <70623172+CrazyApple888@users.noreply.github.com>
2022-03-01 00:54:46 +0700

    Update BicycleWithIssues.kt
    Removed redundant annotation

--- app/src/main/java/ru/nsu/fit/data/models/BicycleWithIssues.kt
+++ app/src/main/java/ru/nsu/fit/data/models/BicycleWithIssues.kt
@@ -5,7 +5,6 @@ import androidx.room.Entity
 import androidx.room.Junction
 import androidx.room.Relation
 
-@Entity
 data class BicycleWithIssues(
     @Embedded val bicycle: Bicycle,
     @Relation(
