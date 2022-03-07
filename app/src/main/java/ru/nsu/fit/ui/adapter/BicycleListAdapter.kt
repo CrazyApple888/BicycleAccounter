1fcc867246fbfaf48994ebb06511cf5baf725e6f
Исаченко Андрей Владимирович <a.isachenko@g.nsu.ru>
2022-03-07 13:23:56 +0700

    UI for home screen, domain models template, adapter template

---
+++ app/src/main/java/ru/nsu/fit/ui/adapter/BicycleListAdapter.kt
@@ -0,0 +1,20 @@
+package ru.nsu.fit.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.nsu.fit.ui.viewholder.BicycleViewHolder
+
+class BicycleListAdapter: RecyclerView.Adapter<BicycleViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BicycleViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BicycleViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
